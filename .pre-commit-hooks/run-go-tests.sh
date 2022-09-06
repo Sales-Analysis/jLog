b47fb981f51d2a0e4674c89a4c7398a6cbcc90bd
pandamy619 <vladislavtagaev@gmail.com>
2022-09-06 23:24:12 +0300

    fix: test sh

--- .pre-commit-hooks/run-go-tests.sh
+++ .pre-commit-hooks/run-go-tests.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
-
-FILES = $(go list ./... | grep -v /vendor/)
+#!/usr/bin/env bash
+FILES=$(go list ./...  | grep -v /vendor/)
 
 go test -tags=unit -timeout 30s -short -v ${FILES}
 
 returncode=$?
 if [ $returncode -ne 0 ]; then
-  echo "tests failed"
+  echo "unit tests failed"
   exit 1
 fi
-
