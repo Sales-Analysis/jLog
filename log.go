4b30b1e27b8db9d05bc3fba446f3f65df752717b
pandamy619 <vladislavtagaev@gmail.com>
2022-10-01 20:53:31 +0300

    feat: color for status log

--- log.go
+++ log.go
@@ -6,6 +6,24 @@ import (
 	"os"
 )
 
+// Status constants
+const (
+	info 	= "INFO"
+	warning = "WARNING"
+	err     = "ERROR"
+)
+
+// status colors
+const (
+	infoColor = "\u001B[34;1m"
+	warningColor = "\u001B[33m"
+	errorColor = "\u001B[31m"
+
+	// reset color
+	resetColor = "\u001B[0m"
+
+)
+
 type jlog struct {
 	location string // Folder with log files
 	format   string // date format
@@ -22,17 +40,17 @@ func Init(location string, format string) *jlog {
 
 // Info calls stdout to print to the logger.
 func (j *jlog) Info(message string) {
-	j.stdout("INFO", message)
+	j.stdout(info, message)
 }
 
 // Warning is equivalent ot Info.
 func (j *jlog) Warning(message string) {
-	j.stdout("WARNING", message)
+	j.stdout(warning, message)
 }
 
 // Error is equivalent ot Info.
 func (j *jlog) Error(message string) {
-	j.stdout("ERROR", message)
+	j.stdout(err, message)
 }
 
 // Stdout writes the output for a logging event.
@@ -40,11 +58,24 @@ func (j *jlog) stdout(prefix string, message string) {
 	if !charEndOfLine(message, "\n") {
 		message = message + "\n"
 	}
-	log := j.logTemplate(timeNow(j.format), prefix, message)
+	p := prefixColor(prefix)
+	log := j.logTemplate(timeNow(j.format), p, message)
 	io.WriteString(os.Stdout, log)
+}
+
+func prefixColor(prefix string) string {
+	switch prefix {
+	case info:
+		return fmt.Sprintf("%s[%s]%s", infoColor, prefix, resetColor)
+	case warning:
+		return fmt.Sprintf("%s[%s]%s", warningColor, prefix, resetColor)
+	case err:
+		return fmt.Sprintf("%s[%s]%s", errorColor, prefix, resetColor)
+	}
+	return prefix
 }
 
 // logTemplate returns a string in a specific format.
 func (j *jlog) logTemplate(date string, prefix string, message string) string {
-	return fmt.Sprintf("[%s][%s]: %s", date, prefix, message)
+	return fmt.Sprintf("[%s]%s: %s", date, prefix, message)
 }
