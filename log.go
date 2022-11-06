1137e390786b9cc4e1bbb0b0fdb8355d6744b653
pandamy619 <vladislavtagaev@gmail.com>
2022-11-06 20:06:36 +0300

    feat: add pkg and fun name to log

--- log.go
+++ log.go
@@ -22,10 +22,11 @@ const (
 	errorColor   = "\u001B[31m"
 	dummyColor   = "\u001B[35m"
 
-	timeColor = "\u001b[32m"
-
+	timeColor    = "\u001b[32m"
+	packageColor = "\u001b[35;1m"
+	funColor     = "\u001b[34;1m"
 	// reset color
-	resetColor = "\u001B[0m"
+	resetColor   = "\u001B[0m"
 )
 
 // default value format log file name
@@ -50,19 +51,19 @@ func Init(location string, format string, filename string) *jlog {
 // Info calls stdout to print to the logger.
 func (j *jlog) Info(message string) {
 	counter, _, _, _ := runtime.Caller(1)
-	j.stdout(dummy, message, counter)
+	j.stdout(info, message, counter)
 }
 
 // Warning is equivalent ot Info.
 func (j *jlog) Warning(message string) {
 	counter, _, _, _ := runtime.Caller(1)
-	j.stdout(dummy, message, counter)
+	j.stdout(warning, message, counter)
 }
 
 // Error is equivalent ot Info.
 func (j *jlog) Error(message string) {
 	counter, _, _, _ := runtime.Caller(1)
-	j.stdout(dummy, message, counter)
+	j.stdout(err, message, counter)
 }
 
 // Dummy is useless log.
@@ -76,12 +77,17 @@ func (j *jlog) stdout(prefix string, message string, counter uintptr) {
 	if !charEndOfLine(message, "\n") {
 		message = message + "\n"
 	}
-	packageName, funcName := getPackageInfo(counter)
-	fmt.Println(packageName, funcName)
-	p := getPrefixColor(prefix)
+	packageName, funName := getPackageInfo(counter)
+	fmt.Println(packageName, funName)
+	
 	t := getTimeColor(timeNow(j.format))
-	log := j.logTemplate(t, p, message)
-	logStdout := j.logTemplateFile(timeNow(j.format), prefix, message)
+	pkg := getColor(packageName, packageColor)
+	fun := getColor(funName, funColor)
+	p := getPrefixColor(prefix)
+
+	log := j.logTemplate(t, pkg, fun, p, message)
+	logStdout := j.logTemplateFile(timeNow(j.format), packageName, funName, prefix, message)
+	
 	io.WriteString(os.Stdout, log)
 	toFile(j.location, j.filename, logStdout)
 }
@@ -102,19 +108,24 @@ func getPrefixColor(prefix string) string {
 	}
 }
 
+// getColor returns the colored string
+func getColor(str string, color string) string {
+	return fmt.Sprintf("%s[%s]%s", color, str, resetColor)
+}
+
 // getTimeColor returns the colored time
 func getTimeColor(time string) string {
 	return fmt.Sprintf("%s[%s]%s", timeColor, time, resetColor)
 }
 
 // logTemplate returns a string in a specific format.
-func (j *jlog) logTemplate(date string, prefix string, message string) string {
-	return fmt.Sprintf("%s%s: %s", date, prefix, message)
+func (j *jlog) logTemplate(date string, pkg string, fun string, prefix string, message string) string {
+	return fmt.Sprintf("%s%s%s%s: %s", date, pkg, fun, prefix, message)
 }
 
 // logTemplateFile returns a string in a specific format.
-func (j *jlog) logTemplateFile(date string, prefix string, message string) string {
-	return fmt.Sprintf("[%s][%s]: %s", date, prefix, message)
+func (j *jlog) logTemplateFile(date string, pkg string, fun string, prefix string, message string) string {
+	return fmt.Sprintf("[%s][%s][%s][%s]: %s", date, pkg, fun, prefix, message)
 }
 
 // toFile write log to file
