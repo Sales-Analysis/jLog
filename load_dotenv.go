85fc41b1c95249c7e870ce1438b3d3dab1760d82
pandamy619 <vladislavtagaev@gmail.com>
2022-12-04 01:21:48 +0300

    refactor: mv to load dotenv

---
+++ load_dotenv.go
@@ -0,0 +1,36 @@
+package jlog
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Sales-Analysis/jLog/internal/dotenv"
+)
+
+// Default parameters
+var defaultParameters = map[string]string{
+	// Folder with log files
+	"LOCATION": "logger",
+	// Format log file name
+	"FORMAT_FILENAME": "20060102",
+	// Format log
+	"FORMAT_TIME_LOG": "2006-01-02 15:04:05",
+	// Separator
+	"SEPARATOR": "[]",
+}
+
+func loadDotEnv(envFile string) {
+	_ = dotenv.Load(envFile)
+	setDefaultParams()
+}
+
+// Set default parameters.
+func setDefaultParams() {
+	for key, value := range defaultParameters {
+		_, found := os.LookupEnv("a")
+		if !found {
+			fmt.Printf("Parameter %s is not sets. The default parameter is used.\n", key)
+			os.Setenv(key, value)
+		}
+	}
+}
